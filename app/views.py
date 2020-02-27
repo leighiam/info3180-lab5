3a058a162547b23805c2d0a0a6055554b1b4f784
Ashleigh Barker <barkerashleigh1@gmail.com>
2020-02-27 11:46:53 -0500

    Lab 5

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash()
 
 
 ###
@@ -34,20 +35,23 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        if form.validate_on_submit():
             # Get the username and password values from the form.
-
+             username = form.username.data
+             password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
-
+            user = UserProfile.query.filter_by(username=username).first()
+            if user is not None and check_password_hash(user.password, password):
             # get user id, load into session
-            login_user(user)
+                login_user(user)
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash("Login Successful")
+            return redirect(url_for('secure_page'))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
@@ -56,6 +60,13 @@ def login():
 @login_manager.user_loader
 def load_user(id):
     return UserProfile.query.get(int(id))
+
+@app.route('/secure-page/')
+def secure_page():
+    if not session.get('logged_in'):
+        return redirect('/')
+    return render_template('secure_page.html')
+    
 
 ###
 # The functions below should be applicable to all Flask apps.
